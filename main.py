75832ee0a48d346b838da5b0f037ffa56762d09c
eugenechou <eugene668822@yahoo.com.tw>
2020-01-07 02:28:29 +0800

    init commit

---
+++ main.py
@@ -0,0 +1,170 @@
+import argparse
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms
+from PIL import Image
+
+from model import ImageTransformNet
+from model import LossNetwork
+
+def load_image(filename, size=None, scale=None):
+    img = Image.open(filename)
+    if size is not None:
+        img = img.resize((size, size), Image.ANTIALIAS)
+    elif scale is not None:
+        img = img.resize((int(img.size[0] / scale), int(img.size[1] / scale)), Image.ANTIALIAS)
+    return img
+
+def save_image(filename, data):
+    img = data.clone().clamp(0, 255).numpy()
+    img = img.transpose(1, 2, 0).astype("uint8")
+    img = Image.fromarray(img)
+    img.save(filename)
+
+def gram_matrix(y):
+    (b, ch, h, w) = y.size()
+    features = y.view(b, ch, w * h)
+    features_t = features.transpose(1, 2)
+    gram = features.bmm(features_t) / (ch * h * w)
+    return gram
+
+def normalize_batch(batch):
+    # normalize using imagenet mean and std
+    mean = batch.new_tensor([0.485, 0.456, 0.406]).view(-1, 1, 1)
+    std = batch.new_tensor([0.229, 0.224, 0.225]).view(-1, 1, 1)
+    batch = batch.div_(255.0)
+    return (batch - mean) / std
+
+def train(args):
+    device = torch.device("cpu")
+    torch.manual_seed(args.seed)
+
+    # Create dataloader for the training data
+    transform = transforms.Compose([
+        transforms.Resize(args.image_size),
+        transforms.CenterCrop(args.image_size),
+        transforms.ToTensor(),
+        transforms.Lambda(lambda x: x.mul(255))
+    ])
+    train_dataset = datasets.ImageFolder(args.dataset, transform)
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size)
+
+    # Load style image
+    style_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Lambda(lambda x: x.mul(255))
+    ])
+    style_image = load_image(args.style_image, size=args.style_size)
+    style = style_transform(style_image)
+    style = style.repeat(args.batch_size, 1, 1, 1).to(device)
+
+    # Defines networks
+    transform_net = ImageTransformNet().to(device)
+    loss_net = LossNetwork().to(device)
+
+    # Define optimizer and loss
+    optimizer = Adam(transformer.parameters(), args.lr)
+    mse_loss = torch.nn.MSELoss().to(device)
+
+    # Extract style features
+    features_style = vgg(normalize_batch(style))
+    gram_style = [gram_matrix(y) for y in features_style]
+
+    for epoch in range(args.epochs):
+        epoch_metrics = {"content": [], "style": [], "total": []}
+        for batch_i, (images, _) in enumerate(dataloader):
+            optimizer.zero_grad()
+
+            images_original = images.to(device)
+            images_transformed = transformer(images_original)
+
+            # Extract features
+            features_original = vgg(images_original)
+            features_transformed = vgg(images_transformed)
+            features_original = normalize_batch(features_original)
+            features_transformed = normalize_batch(features_transformed)
+
+            # Compute content loss as MSE between features
+            content_loss = args.content_weight * mse_loss(features_original.relu2_2, features_transformed.relu2_2)
+
+            # Compute style loss as MSE between gram matrices
+            style_loss = 0.
+            for ft_y, gm_s in zip(features_transformed, gram_style):
+                gm_y = gram_matrix(ft_y)
+                style_loss += mse_loss(gm_y, gm_s[:images.size(0), :, :])
+            style_loss *= args.style_weight
+
+            total_loss = content_loss + style_loss
+            total_loss.backward()
+            optimizer.step()
+
+            epoch_metrics["content"] += [content_loss.item()]
+            epoch_metrics["style"] += [style_loss.item()]
+            epoch_metrics["total"] += [total_loss.item()]
+
+            sys.stdout.write(
+                "\r[Epoch %d/%d] [Batch %d/%d] [Content: %.2f (%.2f) Style: %.2f (%.2f) Total: %.2f (%.2f)]"
+                % (
+                    epoch + 1,
+                    args.epochs,
+                    batch_i,
+                    len(train_dataset),
+                    content_loss.item(),
+                    np.mean(epoch_metrics["content"]),
+                    style_loss.item(),
+                    np.mean(epoch_metrics["style"]),
+                    total_loss.item(),
+                    np.mean(epoch_metrics["total"]),
+                )
+            )
+
+def run(args):
+    device = torch.device("cpu")
+
+    content_image = load_image(args.content_image)
+    content_transform = transforms.Compose([
+        transforms.ToTensor(),                  # [0, 255] -> [0.0, 1.0]
+        transforms.Lambda(lambda x: x.mul(255)) # mul 255
+    ])
+    content_image = content_transform(content_image)
+    content_image = content_image.unsqueeze(0).to(device)
+    
+    with torch.no_grad():
+        style_model = ImageTransformNet()
+        state_dict = torch.load(args.model)
+        style_model.load_state_dict(state_dict)
+        style_model.to(device)
+        output = style_model(content_image).cpu()
+    
+    save_image(args.output_image, output[0])
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(description="parser for fast-neural-style")
+    subparsers = arg_parser.add_subparsers(title="subcommands", dest="subcommand")
+    train_arg_parser = subparsers.add_parser("train", help="parser for training arguments")
+    train_arg_parser.add_argument("--epochs", type=int, default=2)
+    train_arg_parser.add_argument("--batch-size", type=int, default=4)
+    train_arg_parser.add_argument("--dataset", type=str, required=True)
+    train_arg_parser.add_argument("--style-image", type=str, default="images/style_image/udnie.jpg")
+    train_arg_parser.add_argument("--save-model-dir", type=str, required=True)
+    train_arg_parser.add_argument("--seed", type=int, default=42)
+    train_arg_parser.add_argument("--lr", type=float, default=1e-3)
+    train_arg_parser.add_argument("--content-weight", type=float, default=1e5)
+    train_arg_parser.add_argument("--style-weight", type=float, default=1e10)
+    train_arg_parser.add_argument("--image-size", type=int, default=256)
+    train_arg_parser.add_argument("--style-size", type=int, default=None)
+
+    eval_arg_parser = subparsers.add_parser("eval", help="parser for evaluation/stylizing arguments")
+    eval_arg_parser.add_argument("--content-image", type=str, required=True)
+    eval_arg_parser.add_argument("--output-image", type=str, required=True)
+    eval_arg_parser.add_argument("--model", type=str, required=True)
+
+    args = arg_parser.parse_args()
+    if args.subcommand is None:
+        print("ERROR: specify either train or eval")
+        sys.exit(1)
+    if args.subcommand == "train":
+        train(args)
+    else:
+        run(args)
---
+++ model.py
@@ -0,0 +1,99 @@
+import torch
+from  torchvision import models
+from collections import namedtuple
+
+class ImageTransformNet(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        # convolution layers
+        self.conv1 = ConvLayer(3, 32, 9, 1)
+        self.norm1 = torch.nn.InstanceNorm2d(32, affine=True)
+        self.conv2 = ConvLayer(32, 64, 3, 2)
+        self.norm2 = torch.nn.InstanceNorm2d(64, affine=True)
+        self.conv3 = ConvLayer(64, 128, 3, 2)
+        self.norm3 = torch.nn.InstanceNorm2d(128, affine=True)
+        # Residual layers
+        self.res1 = ResidualBlock(128)
+        self.res2 = ResidualBlock(128)
+        self.res3 = ResidualBlock(128)
+        self.res4 = ResidualBlock(128)
+        self.res5 = ResidualBlock(128)
+        # deconvolution layers 
+        self.deconv1 = ConvLayer(128, 64, 3, 2)
+        self.norm1 = torch.nn.InstanceNorm2d(64, affine=True)
+        self.deconv2 = ConvLayer(64, 32, 3, 2)
+        self.norm2 = torch.nn.InstanceNorm2d(32, affine=True)
+        self.deconv3 = ConvLayer(32, 3, 3, 2)
+        self.relu = torch.nn.ReLU()
+
+    def forward(self, x):
+        img = self.relu(self.in1(self.conv1(x)))
+        img = self.relu(self.in2(self.conv2(img)))
+        img = self.relu(self.in3(self.conv3(img)))
+        img = self.res1(img)
+        img = self.res2(img)
+        img = self.res3(img)
+        img = self.res4(img)
+        img = self.res5(img)
+        img = self.relu(self.in4(self.deconv1(img)))
+        img = self.relu(self.in5(self.deconv2(img)))
+        img = self.deconv3(img)
+        return img
+
+class ResidualBlock(torch.nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.conv1 = ConvLayer(channels, channels, kernel_size=3, stride=1)
+        self.in1 = torch.nn.InstanceNorm2d(channels, affine=True)
+        self.conv2 = ConvLayer(channels, channels, kernel_size=3, stride=1)
+        self.in2 = torch.nn.InstanceNorm2d(channels, affine=True)
+        self.relu = torch.nn.ReLU()
+
+    def forward(self, x):
+        residual = x
+        out = self.relu(self.in1(self.conv1(x)))
+        out = self.in2(self.conv2(out))
+        out = self.relu(out + residual)
+        return out
+
+class ConvLayer(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride):
+        super().__init__()
+        self.conv = torch.nn.Sequential(
+            torch.nn.ReflectionPad2d(kernel_size//2),
+            torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride) 
+        )
+    
+    def forward(self, x):
+        out = conv(x)
+        return out
+
+class LossNetwork(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        vgg_pretrained_features = models.vgg16(pretrained=True).features
+        self.slice1 = torch.nn.Sequential()
+        self.slice2 = torch.nn.Sequential()
+        self.slice3 = torch.nn.Sequential()
+        self.slice4 = torch.nn.Sequential()
+        for x in range(4):
+            self.slice1.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(4, 9):
+            self.slice2.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(9, 16):
+            self.slice3.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(16, 23):
+            self.slice4.add_module(str(x), vgg_pretrained_features[x])
+    
+    def forward(self, x):
+        h = self.slice1(x)
+        relu1_2 = h
+        h = self.slice2(h)
+        relu2_2 = h
+        h = self.slice3(h)
+        relu3_3 = h
+        h = self.slice4(h)
+        relu4_3 = h
+        vgg_outputs = namedtuple("VggOutputs", ['relu1_2', 'relu2_2', 'relu3_3', 'relu4_3'])
+        out = vgg_outputs(relu1_2, relu2_2, relu3_3, relu4_3)
+        return out
