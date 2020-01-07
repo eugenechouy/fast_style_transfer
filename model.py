1267651e05c3b21f8eb431be645243cd75a511f9
eugenechouy <eugene668822@yahoo.com.tw>
2020-01-07 16:59:34 +0800

    update

--- model.py
+++ model.py
@@ -1,46 +1,66 @@
 import torch
-from  torchvision import models
+from torchvision import models
 from collections import namedtuple
 
-class ImageTransformNet(torch.nn.Module):
+class TransformerNet(torch.nn.Module):
     def __init__(self):
-        super(ImageTransformNet, self).__init__()
-        # convolution layers
-        self.conv1 = ConvLayer(3, 32, 9, 1)
-        self.norm1 = torch.nn.InstanceNorm2d(32, affine=True)
-        self.conv2 = ConvLayer(32, 64, 3, 2)
-        self.norm2 = torch.nn.InstanceNorm2d(64, affine=True)
-        self.conv3 = ConvLayer(64, 128, 3, 2)
-        self.norm3 = torch.nn.InstanceNorm2d(128, affine=True)
+        super(TransformerNet, self).__init__()
+        # Initial convolution layers
+        self.conv1 = ConvLayer(3, 32, kernel_size=9, stride=1)
+        self.in1 = torch.nn.InstanceNorm2d(32, affine=True)
+        self.conv2 = ConvLayer(32, 64, kernel_size=3, stride=2)
+        self.in2 = torch.nn.InstanceNorm2d(64, affine=True)
+        self.conv3 = ConvLayer(64, 128, kernel_size=3, stride=2)
+        self.in3 = torch.nn.InstanceNorm2d(128, affine=True)
         # Residual layers
         self.res1 = ResidualBlock(128)
         self.res2 = ResidualBlock(128)
         self.res3 = ResidualBlock(128)
         self.res4 = ResidualBlock(128)
         self.res5 = ResidualBlock(128)
-        # deconvolution layers 
-        self.deconv1 = ConvLayer(128, 64, 3, 2)
-        self.norm4 = torch.nn.InstanceNorm2d(64, affine=True)
-        self.deconv2 = ConvLayer(64, 32, 3, 2)
-        self.norm5 = torch.nn.InstanceNorm2d(32, affine=True)
-        self.deconv3 = ConvLayer(32, 3, 3, 2)
+        # Upsampling Layers
+        self.deconv1 = UpsampleConvLayer(128, 64, kernel_size=3, stride=1, upsample=2)
+        self.in4 = torch.nn.InstanceNorm2d(64, affine=True)
+        self.deconv2 = UpsampleConvLayer(64, 32, kernel_size=3, stride=1, upsample=2)
+        self.in5 = torch.nn.InstanceNorm2d(32, affine=True)
+        self.deconv3 = ConvLayer(32, 3, kernel_size=9, stride=1)
+        # Non-linearities
         self.relu = torch.nn.ReLU()
 
+    def forward(self, X):
+        y = self.relu(self.in1(self.conv1(X)))
+        y = self.relu(self.in2(self.conv2(y)))
+        y = self.relu(self.in3(self.conv3(y)))
+        y = self.res1(y)
+        y = self.res2(y)
+        y = self.res3(y)
+        y = self.res4(y)
+        y = self.res5(y)
+        y = self.relu(self.in4(self.deconv1(y)))
+        y = self.relu(self.in5(self.deconv2(y)))
+        y = self.deconv3(y)
+        return y
+
+
+class ConvLayer(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride):
+        super(ConvLayer, self).__init__()
+        reflection_padding = kernel_size // 2
+        self.reflection_pad = torch.nn.ReflectionPad2d(reflection_padding)
+        self.conv2d = torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride)
+
     def forward(self, x):
-        img = self.relu(self.norm1(self.conv1(x)))
-        img = self.relu(self.norm2(self.conv2(img)))
-        img = self.relu(self.norm3(self.conv3(img)))
-        img = self.res1(img)
-        img = self.res2(img)
-        img = self.res3(img)
-        img = self.res4(img)
-        img = self.res5(img)
-        img = self.relu(self.norm4(self.deconv1(img)))
-        img = self.relu(self.norm5(self.deconv2(img)))
-        img = self.deconv3(img)
-        return img
+        out = self.reflection_pad(x)
+        out = self.conv2d(out)
+        return out
+
 
 class ResidualBlock(torch.nn.Module):
+    """ResidualBlock
+    introduced in: https://arxiv.org/abs/1512.03385
+    recommended architecture: http://torch.ch/blog/2016/02/04/resnets.html
+    """
+
     def __init__(self, channels):
         super(ResidualBlock, self).__init__()
         self.conv1 = ConvLayer(channels, channels, kernel_size=3, stride=1)
@@ -53,24 +73,35 @@ class ResidualBlock(torch.nn.Module):
         residual = x
         out = self.relu(self.in1(self.conv1(x)))
         out = self.in2(self.conv2(out))
-        out = self.relu(out + residual)
+        out = out + residual
         return out
 
-class ConvLayer(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride):
-        super(ConvLayer, self).__init__()
-        self.conv = torch.nn.Sequential(
-            torch.nn.ReflectionPad2d(kernel_size//2),
-            torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride) 
-        )
-    
+
+class UpsampleConvLayer(torch.nn.Module):
+    """UpsampleConvLayer
+    Upsamples the input and then does a convolution. This method gives better results
+    compared to ConvTranspose2d.
+    ref: http://distill.pub/2016/deconv-checkerboard/
+    """
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride, upsample=None):
+        super(UpsampleConvLayer, self).__init__()
+        self.upsample = upsample
+        reflection_padding = kernel_size // 2
+        self.reflection_pad = torch.nn.ReflectionPad2d(reflection_padding)
+        self.conv2d = torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride)
+
     def forward(self, x):
-        out = self.conv(x)
+        x_in = x
+        if self.upsample:
+            x_in = torch.nn.functional.interpolate(x_in, mode='nearest', scale_factor=self.upsample)
+        out = self.reflection_pad(x_in)
+        out = self.conv2d(out)
         return out
-
-class LossNetwork(torch.nn.Module):
-    def __init__(self):
-        super(LossNetwork, self).__init__()
+    
+class Vgg16(torch.nn.Module):
+    def __init__(self, requires_grad=False):
+        super(Vgg16, self).__init__()
         vgg_pretrained_features = models.vgg16(pretrained=True).features
         self.slice1 = torch.nn.Sequential()
         self.slice2 = torch.nn.Sequential()
@@ -84,18 +115,19 @@ class LossNetwork(torch.nn.Module):
             self.slice3.add_module(str(x), vgg_pretrained_features[x])
         for x in range(16, 23):
             self.slice4.add_module(str(x), vgg_pretrained_features[x])
-        for param in self.parameters():
-            param.requires_grad = False
-   
-    def forward(self, x):
-        h = self.slice1(x)
-        relu1_2 = h
+        if not requires_grad:
+            for param in self.parameters():
+                param.requires_grad = False
+
+    def forward(self, X):
+        h = self.slice1(X)
+        h_relu1_2 = h
         h = self.slice2(h)
-        relu2_2 = h
+        h_relu2_2 = h
         h = self.slice3(h)
-        relu3_3 = h
+        h_relu3_3 = h
         h = self.slice4(h)
-        relu4_3 = h
+        h_relu4_3 = h
         vgg_outputs = namedtuple("VggOutputs", ['relu1_2', 'relu2_2', 'relu3_3', 'relu4_3'])
-        out = vgg_outputs(relu1_2, relu2_2, relu3_3, relu4_3)
+        out = vgg_outputs(h_relu1_2, h_relu2_2, h_relu3_3, h_relu4_3)
         return out
